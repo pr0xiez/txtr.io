b866ff85d27934295e2eb7920507c9b923c27d7e
hall3ad <hall3ad@mail.uc.edu>
2017-09-15 11:10:14 -0400

    trying

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { platformBrowser }    from '@angular/platform-browser';
 import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
 
 console.log('Running AOT compiled');
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+  platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+});
