e5d11f4a6b451c990b1addb6d28dfe942f6d8a9f
Alex Hall <alex.hallll.03@gmail.com>
2017-10-17 11:15:15 -0400

    customers cards scaffolded and basic styling

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,7 +1,64 @@
 import { Component } from '@angular/core'
 
 @Component({
-  template: `<p>customers page works!</p>`
+  template: `
+  <div fxLayout="row" fxLayoutWrap fxLayoutGap="10px">
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>Name</mat-card-title>
+        <mat-card-subtitle>Email (if exists)</mat-card-subtitle>
+      </mat-card-header>
+      <mat-card-content>
+        <p>
+          id<br>
+          phoneNumber<br>
+          timezone
+        </p>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-button>SENT</button>
+        <button mat-button>QUEUED</button>
+      </mat-card-actions>
+    </mat-card>
+
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>Name</mat-card-title>
+        <mat-card-subtitle>Email (if exists)</mat-card-subtitle>
+      </mat-card-header>
+      <mat-card-content>
+        <p>
+          id<br>
+          phoneNumber<br>
+          timezone
+        </p>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-button>SENT</button>
+        <button mat-button>QUEUED</button>
+      </mat-card-actions>
+    </mat-card>
+    
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>Name</mat-card-title>
+        <mat-card-subtitle>Email (if exists)</mat-card-subtitle>
+      </mat-card-header>
+      <mat-card-content>
+        <p>
+          id<br>
+          phoneNumber<br>
+          timezone
+        </p>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-button>SENT</button>
+        <button mat-button>QUEUED</button>
+      </mat-card-actions>
+    </mat-card>
+  </div>
+  `,
+  styleUrls: ['./customers.component.scss']
 })
 
 export class CustomersComponent {}
