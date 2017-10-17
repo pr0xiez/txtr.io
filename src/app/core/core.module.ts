7ef85548545beb9bbe02c7e42514aa78d75eb555
Alex Hall <alex.hallll.03@gmail.com>
2017-10-17 09:53:21 -0400

    delete whitespace

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,11 @@
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
-import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { AuthService } from "./services/auth.service";
+import { RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { SharedModule } from '../shared/shared.module'
+import { NgModule, Optional, SkipSelf } from '@angular/core'
+import { AuthService } from "./services/auth.service"
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
-import { HttpInterceptorService } from "./http.interceptors";
-import { EnsureModuleLoadedOnceGuard } from './ensureModuleLoadedOnceGuard';
+import { HttpInterceptorService } from "./http.interceptors"
+import { EnsureModuleLoadedOnceGuard } from './ensureModuleLoadedOnceGuard'
 
 /**
  * @author ADH - 9.7.17 - <alex.hall@united-installs.com>
@@ -23,10 +23,7 @@ import { EnsureModuleLoadedOnceGuard } from './ensureModuleLoadedOnceGuard';
 })
 
 export class CoreModule extends EnsureModuleLoadedOnceGuard {    //Ensure that CoreModule is only loaded into AppModule
-  
-  //Looks for the module in the parent injector to see if it's already been loaded (only want it loaded once)
-  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
-    super(parentModule);
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) { //Looks for the module in the parent injector to see if it's already been loaded (only want it loaded once)
+    super(parentModule)
   }  
-
 }
