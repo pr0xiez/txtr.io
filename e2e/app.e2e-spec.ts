d27f906f3d24853e546e138bde0584c493dfff13
Alex Hall <alex.hallll.03@gmail.com>
2017-11-15 14:52:55 -0500

    e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,22 +1,52 @@
 import { AppPage } from './app.po'
 
-describe('txtr.io Login', () => {
+describe('txtr.io', () => {
   let page: AppPage
 
   beforeEach(() => {
     page = new AppPage()
-
-
   })
 
-  it('should take user to login page upon first visit', () => {
+  it('should take user to /login page upon first visit', () => {
     page.navigateToRoot()
-    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49152/login')
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/login')
   })
 
-  it('should navigate directly to texts/type page upon successful login', () => {
+  it('should not let users navigate into site before logging in', () => {
+    page.navigateTo('/texts/type')
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/login')
+    page.navigateTo('/customers')
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/login')
+  })
+
+  it('should navigate directly to /texts/type page upon successful login', () => {
     page.navigateToRoot()
     page.submitLoginFormWithValidCredentials()
-    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49152/texts/type')
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/texts/type')
+  })
+
+  it('should be able to navigate to /customers by hand after user has logged in', () => {
+    page.navigateTo('customers')
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/customers')
+  })
+
+  it('should be able to navigate to /texts/type by hand after user has logged in', () => {
+    page.navigateTo('texts/type')
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/texts/type')
+  })
+
+  it('should be able to navigate to /customers via click nav button', () => {
+    page.clickCustomers()
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/customers')
+  })
+
+  it('should be able to navigate to /texts/type via click nav button', () => {
+    page.clickTextTypes()
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/texts/type')
+  })
+
+  it('should unauthenticate the user on logout', () => {
+    page.clickLogout()
+    expect(page.getCurrentPage().then(x => x)).toEqual('http://localhost:49155/login')
   })
 })
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,6 +5,10 @@ export class AppPage {
     return browser.get('/')
   }
 
+  navigateTo(string: string) {
+    return browser.get(string)
+  }
+
   getCurrentPage() {
     return browser.getCurrentUrl()
   }
@@ -13,5 +17,17 @@ export class AppPage {
     element(by.css("input[formcontrolname='email']")).sendKeys('devAdmin@united-installs.com')
     element(by.css("input[formcontrolname='password']")).sendKeys('United123!')
     element(by.css("button[type='submit']")).click() 
+  }
+
+  clickLogout() {
+    element(by.buttonText('Logout')).click()
+  }
+
+  clickCustomers() {
+    element(by.linkText('Customers')).click()
+  }
+
+  clickTextTypes() {
+    element(by.linkText('Text Types')).click()
   }
 }
